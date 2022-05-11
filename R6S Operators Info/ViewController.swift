fa442a2791138a783a758f24e99e2997c2df5b8b
wickapp <wickhomie@gmail.com>
2022-05-11 10:18:51 +0300

    Initial Commit

---
+++ R6S Operators Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R6S Operators Info
+//
+//  Created by Илья Терновской on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
