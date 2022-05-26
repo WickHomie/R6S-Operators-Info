35a692c59bf9abee29e5f225db0db4e502e28a8d
wickapp <wickhomie@gmail.com>
2022-05-26 17:27:24 +0300

    HW2.8

--- R6S Operators Info/ViewControllers/OperatorInfoVC.swift
+++ R6S Operators Info/ViewControllers/OperatorInfoVC.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class OperatorInfoVC: UIViewController {
+    
+    @IBOutlet weak var imageOperator: UIImageView!
+    @IBOutlet weak var personalInfoOperator: UILabel!
+    @IBOutlet weak var healthLevel: UILabel!
+    @IBOutlet weak var speedLevel: UILabel!
+    @IBOutlet weak var biographyOperator: UILabel!
+    @IBOutlet weak var birthdayOperator: UILabel!
+    @IBOutlet weak var birthplaceOperator: UILabel!
+    
+    var attackInfo: Attack!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background5")
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        imageOperator.image = UIImage(named: attackInfo.imageFull)
+        birthdayOperator.text = attackInfo.birthday
+        birthplaceOperator.text = attackInfo.birthplace
+        personalInfoOperator.text = "\(attackInfo.fullName)"
+        healthLevel.text = attackInfo.health
+        speedLevel.text = attackInfo.speed
+        biographyOperator.text = attackInfo.bio
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- R6S Operators Info/ViewControllers/OperatorsVC.swift
+++ R6S Operators Info/ViewControllers/OperatorsVC.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class OperatorsVC: UITableViewController {
     
-    private var attack = Attack.getAttack()
+
+    var attack = Attack.getAttack()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,55 +39,14 @@ class OperatorsVC: UITableViewController {
        
         cell.contentConfiguration = content
         
-        
-
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let attackInfoVC = segue.destination as? OperatorInfoVC else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        let attack = attack[indexPath.row]
+        attackInfoVC.attackInfo = attack
     }
-    */
 
 }
